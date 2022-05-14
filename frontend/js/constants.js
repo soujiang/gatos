ad1afbd3762daafabfb25ba073e0d881ddee60ad
soujiang <soujiangelecthrons@gmail.com>
2022-05-14 18:35:45 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x26E4c9Efd5168fBE8Ee443e8407439eaF0ad5CD2"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
