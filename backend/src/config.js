ef6ac25b3002ad4e8e6266a91f24df3112c2ad94
soujiang <soujiangelecthrons@gmail.com>
2022-05-14 18:21:11 -0400

    contract

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x26E4c9Efd5168fBE8Ee443e8407439eaF0ad5CD2"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
